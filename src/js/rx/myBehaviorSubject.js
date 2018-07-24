1ff01828703eaafdc473aa27c697c3cb69693972
adile <Garfield00>
2018-07-24 10:28:30 +0300

    Added comments and removed inheritance between types.

--- src/js/rx/myBehaviorSubject.js
+++ src/js/rx/myBehaviorSubject.js
@@ -1,8 +1,14 @@
-import { MyObservable } from "./myObservable";
+import MyObservable from './MyObservable';
 
-class MyBehaviorSubject extends MyObservable {
+/**
+ * An RxJS-like implementation of an BehaviorSubject
+ */
+class MyBehaviorSubject {
+    /**
+     * set the initial value
+      * @param initialValue
+     */
     constructor(initialValue) {
-        super();
         this.observers = [];
 
         if (typeof initialValue === 'undefined') {
@@ -15,8 +21,14 @@ class MyBehaviorSubject extends MyObservable {
 
     subscribe(observer) {
         this.observers.push(observer);
+        observer(this.lastValue);
     }
 
+    /**
+     * update the value and save the last one in the history array
+      * @param value
+     * @param dontAddToHistory
+     */
     next(value, dontAddToHistory) {
         if (!dontAddToHistory) {
             this.history.push(Object.assign({}, this.lastValue));
@@ -26,15 +38,31 @@ class MyBehaviorSubject extends MyObservable {
         this.observers.forEach(observer => observer(value));
     }
 
+    /**
+     * get a copy of the current value
+      * @returns {*}
+     */
     getValue() {
         return jQuery.extend(true, {}, this.lastValue);
     }
 
+    /**
+     * set the state with the previous value
+      */
     undo() {
         if (this.history.length) {
             let value = this.history.pop();
             this.next(value, true);
         }
+    }
+
+    /**
+     * Create a new MyObservable with the current observers and trigger the next()
+     */
+    asObservable() {
+        let observable = MyObservable(this.observers);
+        observable.next(this.lastValue);
+        return observable();
     }
 }
 
--- src/js/rx/myObservable.js
+++ src/js/rx/myObservable.js
@@ -1,21 +1,42 @@
-
+/**
+ * An RxJS-like implementation of an observable which maintains a list of subscribers.
+ */
 class MyObservable {
-    constructor() {
-        this.observers = [];
+    /**
+     * Init the object
+     * @param observers - optional array of observers
+     */
+    constructor(observers) {
+        this.observers = Array.isArray(observers) ? observers : [];
     }
 
+    /**
+     * Add a new subscriber
+     * @param observer
+     */
     subscribe(observer) {
         this.observers.push(observer);
     }
 
+    /**
+     * Trigger all subscribers with the value
+     * @param value
+     */
     next(value) {
         this.observers.forEach(observer => observer.next(value));
     }
 
+    /**
+     * Trigger all subscribers with an error
+     * @param error
+     */
     error(error) {
         this.observers.forEach(observer => observer.error(error));
     }
 
+    /**
+     * Complete all subscribers
+     */
     complete() {
         this.observers.forEach(observer => observer.complete());
     }
